fb0788bc9b31a1f7bf44ec5f853605804cb53375
wanglei <droidlover@126.com>
2017-02-07 11:42:43 +0800

    add 下拉刷新切换的demo

--- app/src/main/java/cn/droidlover/xstatecontroller/demo/MainActivity.java
+++ app/src/main/java/cn/droidlover/xstatecontroller/demo/MainActivity.java
@@ -1,6 +1,7 @@
 package cn.droidlover.xstatecontroller.demo;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button bt_showError;
     Button bt_showEmpty;
     Button bt_showContent;
+
+    SwipeRefreshLayout swipeLayout;
 
     XStateController controller;
 
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt_showContent = (Button) findViewById(R.id.bt_showContent);
         controller = (XStateController) findViewById(R.id.controller);
 
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipeLayout);
+
         bt_showLoading.setOnClickListener(this);
         bt_showEmpty.setOnClickListener(this);
         bt_showError.setOnClickListener(this);
@@ -33,6 +38,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         controller.loadingView(View.inflate(this, R.layout.view_loading, null));
         controller.errorView(View.inflate(this, R.layout.view_error, null));
+
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                controller.showLoading();
+                swipeLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeLayout.setRefreshing(false);
+                        controller.showContent();
+                    }
+                }, 2000L);
+            }
+        });
 
         controller.registerStateChangeListener(new XStateController.SimpleStateChangeListener() {
 
